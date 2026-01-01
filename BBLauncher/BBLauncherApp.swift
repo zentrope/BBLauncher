df89b647300c02cc3d1c2780b8666f1786e94410
Keith Irwin <keith@zentrope.com>
2025-12-31 19:15:57 -0800

    Initial Commit

---
+++ BBLauncher/BBLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBLauncherApp.swift
+//  BBLauncher
+//
+//  Created by Keith Irwin on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct BBLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBLauncher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BBLauncher
+//
+//  Created by Keith Irwin on 12/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
